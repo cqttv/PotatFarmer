08f3a780a0bd00e4aa136426205fb4e95277288b
CatQuery <git@catquery.com>
2026-08-15 15:50:43 -0400

    chore: lint

--- tests/dashboard.test.ts
+++ tests/dashboard.test.ts
@@ -3,7 +3,6 @@ import test from "node:test";
 
 import { DASHBOARD_HTML } from "../src/http/dashboard.js";
 
-
 test("dashboard element IDs are unique", () => {
   const ids = [...DASHBOARD_HTML.matchAll(/\sid="([^"]+)"/g)].map(
     (match) => match[1],
